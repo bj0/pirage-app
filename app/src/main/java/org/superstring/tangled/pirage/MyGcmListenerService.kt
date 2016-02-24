3f032f67cd9ded2b72c9a12bec85754714c6a0ef
Brian Parma <bparma@bdatech.com>
2016-02-24 16:30:18 -0700

    make doNotify callable outside GCM service

--- app/src/main/java/org/superstring/tangled/pirage/MyGcmListenerService.kt
+++ app/src/main/java/org/superstring/tangled/pirage/MyGcmListenerService.kt
@@ -3,13 +3,17 @@ package org.superstring.tangled.pirage
 import android.app.Notification
 import android.app.PendingIntent
 import android.app.TaskStackBuilder
+import android.content.Context
 import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.text.format.Time
 import com.google.android.gms.gcm.GcmListenerService
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.intentFor
 import org.jetbrains.anko.notificationManager
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * Created by Brian Parma on 1/27/16.
@@ -33,28 +37,30 @@ class MyGcmListenerService : GcmListenerService(), AnkoLogger {
             doNotify(isOpen)
         }
     }
+}
 
-    private val notificationId = 2553
+private val notificationId = 2553
 
-    private fun doNotify(isOpen: Boolean) {
-        val message = "Garage ${if (isOpen) "Open!" else "Closed!"}"
-        val largeIcon = BitmapFactory.decodeResource(resources, if ( isOpen) R.drawable.open else R.drawable.closed)
+fun Context.doNotify(isOpen: Boolean) {
+    val sdf = SimpleDateFormat("HH:mm:ss")
+    val message = "Garage ${if (isOpen) "Open!" else "Closed!"}"
+    val largeIcon = BitmapFactory.decodeResource(resources, if ( isOpen) R.drawable.open else R.drawable.closed)
 
-        val notification = Notification.Builder(this)
-                .setLargeIcon(largeIcon)
-                .setSmallIcon(R.drawable.icon)
-                .setContentTitle("Pirage!")
-                .setContentText(message)
-                .setPriority(Notification.PRIORITY_HIGH)
-                .addAction(
-                        if (isOpen) R.mipmap.close_icon else R.mipmap.open_icon,
-                        if ( isOpen ) "Close" else "Open", PendingIntent.getService(this, 0, intentFor<ClickService>(), 0))
+    val notification = Notification.Builder(this)
+            .setLargeIcon(largeIcon)
+            .setSmallIcon(R.drawable.icon)
+            .setContentTitle("Pirage!")
+            .setContentText(message)
+            .setPriority(Notification.PRIORITY_HIGH)
+            .setDefaults(Notification.DEFAULT_VIBRATE)
+            .addAction(
+                    if (isOpen) R.mipmap.close_icon else R.mipmap.open_icon,
+                    if ( isOpen ) "Close" else "Open", PendingIntent.getService(this, 0, intentFor<ClickService>(), 0))
 
-        val stackBuilder = TaskStackBuilder.create(this)
-        stackBuilder.addParentStack(MainActivity::class.java)
-        stackBuilder.addNextIntent(intentFor<MainActivity>())
-        notification.setContentIntent(stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT))
+    val stackBuilder = TaskStackBuilder.create(this)
+    stackBuilder.addParentStack(MainActivity::class.java)
+    stackBuilder.addNextIntent(intentFor<MainActivity>())
+    notification.setContentIntent(stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT))
 
-        notificationManager.notify(notificationId, notification.build())
-    }
+    notificationManager.notify(notificationId, notification.build())
 }
