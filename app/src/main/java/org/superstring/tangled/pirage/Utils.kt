2a2bf5afa7de9b18111bc6c2f95584ecca6aec1c
Brian Parma <execrable@gmail.com>
2018-03-14 18:40:04 -0700

    add utils

---
+++ app/src/main/java/org/superstring/tangled/pirage/Utils.kt
@@ -0,0 +1,10 @@
+package org.superstring.tangled.pirage
+
+import org.jetbrains.anko.toast
+import splitties.init.appCtx
+
+/**
+ * Created by bjp on 3/1/18.
+ */
+
+fun toast(message: String) = appCtx.toast(message)
